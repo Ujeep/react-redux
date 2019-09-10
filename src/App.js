5913bf554ce8a07f88ff521a1069c36020914f27
uji <uji_yusuke@giginc.co.jp>
2019-09-10 19:26:42 +0900

    15-props

--- src/App.js
+++ src/App.js
@@ -10,18 +10,28 @@ import React from 'react';
 // }
 
 const App = () => {
+  const profiles = [
+    { name: "taro", age: 10 },
+    { name: "hanako", age: 5 },
+    { name: "noName" }
+  ]
   return (
-   <div>
-     <Cat />
-     <Cat />
-     <Cat />
-     <Cat />
-   </div> 
+    <div>
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
+    </div>
   )
 }
 
-const Cat = () => {
-  return <div>Meow!</div>
+const User = (props) => {
+  return <div>Hi,I am{props.name},and {props.age} years old</div>
+}
+
+User.defaultProps = {
+  age: 1
 }
 
 export default App;
