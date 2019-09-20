575ebb27fa56190f00a0d0a7727ca168787a54a7
uji <uji_yusuke@giginc.co.jp>
2019-09-20 14:47:55 +0900

    implement action and action-creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREAMENT = 'INCREAMENT'
+export const DECREAMENT = 'DECREAMENT'
+
+export const increment = () => ({
+    type: INCREAMENT
+})
+
+export const decrement = () => ({
+    type: DECREAMENT
+})
